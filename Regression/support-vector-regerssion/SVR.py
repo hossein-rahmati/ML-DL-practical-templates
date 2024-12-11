d57b4ca4664fd94b3fe96cea96cb2734faa33fd6
hossein-rahmati <rmthossein@gmail.com>
2024-12-11 17:05:15 +0330

    showing SVR results on plot

--- Regression/support-vector-regerssion/SVR.py
+++ Regression/support-vector-regerssion/SVR.py
@@ -13,12 +13,37 @@ y = dataset.iloc[:, -1].values
 # %% reshaping
 y = y.reshape(len(y), 1)
 
-# %%feature scaling
+# %% feature scaling
 sc_X = StandardScaler()
 sc_y = StandardScaler()
 X = sc_X.fit_transform(X)
 y = sc_y.fit_transform(y)
 
-# %%training the SVR model on the whole dataset
+# %% training the SVR model on the whole dataset
 regressor = SVR(kernel='rbf')
 regressor.fit(X, y)
+
+# %% training new results
+sc_y.inverse_transform(regressor.predict(
+    sc_X.transform([[6.5]])).reshape(-1, 1))
+
+# %% visualising the SVR results
+plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color='red')
+plt.plot(sc_X.inverse_transform(X), sc_y.inverse_transform(
+    regressor.predict(X).reshape(-1, 1)), color='blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+# %% visualising the SVR results (for higher resolution and smoother curve)
+X_grid = np.arange(min(sc_X.inverse_transform(X)),
+                   max(sc_X.inverse_transform(X)), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color='red')
+plt.plot(X_grid, sc_y.inverse_transform(
+    regressor.predict(sc_X.transform(X_grid)).reshape(-1, 1)), color='blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
